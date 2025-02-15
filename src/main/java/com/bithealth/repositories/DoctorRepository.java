5e8ee5ed63a61eba47f7a51d20a9af995dac563f
ONG JIA YUAN <ongjy98@outlook.com>
2025-02-15 10:50:40 +0800

    Appointment and prescription services configured

--- src/main/java/com/bithealth/repositories/DoctorRepository.java
+++ src/main/java/com/bithealth/repositories/DoctorRepository.java
@@ -1,6 +1,7 @@
 package com.bithealth.repositories;
 
 import com.bithealth.entities.Doctor;
+import com.bithealth.entities.Patient;
 import com.bithealth.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
