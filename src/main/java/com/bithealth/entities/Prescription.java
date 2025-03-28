36ac261a587feec56254934e8d52179b47ba0ab6
Jong Yann <jonggietechie@gmail.com>
2025-03-28 22:21:23 +0800

    Fix Prescription persistance

--- src/main/java/com/bithealth/entities/Prescription.java
+++ src/main/java/com/bithealth/entities/Prescription.java
@@ -4,17 +4,17 @@ import com.bithealth.helpers.MedicineItemListConverter;
 import com.bithealth.dto.MedicineItem;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Entity
 @Table(name = "prescriptions")
 @EntityListeners(AuditingEntityListener.class)
-
 public class Prescription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +25,7 @@ public class Prescription {
     private Appointment appointment;
 
     @Convert(converter = MedicineItemListConverter.class)
+    @JdbcTypeCode(SqlTypes.JSON)
     @Column(name = "medicine_list", columnDefinition = "jsonb")
     private List<MedicineItem> medicineList;
 
