157a0938de27539280ced1f22e845e15db2dc454
Jong Yann <jonggietechie@gmail.com>
2025-04-06 22:13:30 +0800

    Update Prescription and Medical Certificate

--- src/main/java/com/bithealth/dto/MedicalCertificateVerificationDTO.java
+++ src/main/java/com/bithealth/dto/MedicalCertificateVerificationDTO.java
@@ -1,9 +1,11 @@
 package com.bithealth.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 public class MedicalCertificateVerificationDTO {
+    @JsonFormat(pattern = "M/d/yyyy, h:mm:ss a")
     private LocalDateTime lastVerified;
 }
