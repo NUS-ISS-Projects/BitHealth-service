e3cd19d287be3f89e2b4524ba8f572a318f9c252
ONG JIA YUAN <ongjy98@outlook.com>
2025-04-15 21:46:08 +0800

    Use firebase authentication to decode token and validate

--- src/main/java/com/bithealth/repositories/PatientRepository.java
+++ src/main/java/com/bithealth/repositories/PatientRepository.java
@@ -3,12 +3,13 @@ package com.bithealth.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bithealth.entities.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Optional<Patient> findById(Long patientId);
-
-    Optional<Patient> findByUserId(Long patientId);
+    @Query("SELECT p FROM Patient p WHERE p.user.userId = :userId")
+    Optional<Patient> findByUserId(Long userId);
 }
--- src/main/java/com/bithealth/utils/FirebaseTokenValidator.java
+++ src/main/java/com/bithealth/utils/FirebaseTokenValidator.java
@@ -1,10 +1,5 @@
 package com.bithealth.utils;
 
-import java.util.Collections;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,21 +8,16 @@ import com.google.firebase.auth.FirebaseToken;
 @Component
 public class FirebaseTokenValidator {
 
-    public Authentication validateToken(String token) {
+    public String validateTokenAndGetEmail(String token) {
         try {
             // Verify the Firebase token
             FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
 
             // Extract user details from the token
-            String uid = decodedToken.getUid();
             String email = decodedToken.getEmail();
 
             // Create an Authentication object
-            return new UsernamePasswordAuthenticationToken(
-                uid,
-                null,
-                Collections.singletonList(new SimpleGrantedAuthority("USER"))
-            );
+            return email;
         } catch (Exception e) {
             throw new RuntimeException("Invalid token", e);
         }
