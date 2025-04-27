4541c5faf17fce51253a0176001a93cc3f6e5c82
ONG JIA YUAN <ongjy98@outlook.com>
2025-04-27 15:57:28 +0800

    Test fix

--- src/test/java/com/bithealth/BitHealthApplicationTests.java
+++ src/test/java/com/bithealth/BitHealthApplicationTests.java
@@ -1,12 +1,8 @@
 package com.bithealth;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BitHealthApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 }
